b428d9f141d5eb9fc3e7d74b6598fdb31dca8d82
Sachin S <singh.sacin2702@gmail.com>
2022-07-09 16:57:00 +0530

    port changed to 3000

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use((err, req, res, next) => {
 
 
 
-app.listen(5000,()=>{
+app.listen(3000,()=>{
     connect();
-    console.log('Server is running on port 5000')
+    console.log('Server is running on port 3000')
     })
